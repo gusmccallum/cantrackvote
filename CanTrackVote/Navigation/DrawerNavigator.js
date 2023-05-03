9738efe1e1c08c1c8c5d664c0ef7db0afe077050
gusmccallum <gusmccallum@hotmail.com>
2023-05-02 20:12:36 -0400

    Settling on this navigation structure

--- CanTrackVote/Navigation/DrawerNavigator.js
+++ CanTrackVote/Navigation/DrawerNavigator.js
@@ -3,7 +3,8 @@ import { View } from "react-native";
 import { createDrawerNavigator } from "@react-navigation/drawer";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native-gesture-handler';
-import TabNavigator from "./TabNavigator";
+import { ActivityFeedTab, SearchScreenTab, IssuesScreenTab } from "./TabNavigator";
+
 
 const Drawer = createDrawerNavigator();
 
@@ -15,18 +16,22 @@ const DrawerNavigator = () => {
           backgroundColor: '#3f7819', // Set header background color
         },
         headerTitle: 'CanTrackVote', // Set header title
+        headerTitleStyle: {
+          color: '#FFF', // Set header title color to white
+        },
+        headerTitleAlign: 'center', // Center the header title
+        headerRight: () => (
+          <View>
+            <TouchableOpacity style={{marginLeft:15}}>
+              <MaterialCommunityIcons name='dots-vertical' size={28} color='#000'/>
+            </TouchableOpacity>          
+          </View>
+        )
       }}>
-      <Drawer.Screen 
-        name="Home" 
-        component={TabNavigator} 
-        options={{
-          headerRight: () => (
-            <View>
-              <TouchableOpacity style={{marginLeft:15}}>
-                <MaterialCommunityIcons name='dots-vertical' size={28} color='#000'/>
-              </TouchableOpacity>          
-            </View>
-          )}}/>
+      <Drawer.Screen name="Home" component={ActivityFeedTab}/>
+      <Drawer.Screen name="Search" component={SearchScreenTab}/>
+      <Drawer.Screen name="Issues" component={IssuesScreenTab}/>
+          
     </Drawer.Navigator>
   );
 }
--- CanTrackVote/Navigation/TabNavigator.js
+++ CanTrackVote/Navigation/TabNavigator.js
@@ -2,19 +2,21 @@ import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import { MainStackNavigator, SearchStackNavigator, IssuesStackNavigator } from "./StackNavigator";
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import ActivityFeedScreen from "../Screens/ActivityFeedScreen";
+import SearchScreen from "../Screens/SearchScreen";
+import IssuesScreen from "../Screens/IssuesScreen";
 
 
 const Tab = createBottomTabNavigator();
 
-const BottomTabNavigator = () => {
+const ActivityFeedTab = () => {
   return (
-    <Tab.Navigator
+    <Tab.Navigator initialRouteName="ActivityFeed"
     screenOptions={{headerShown: false}}>
       <Tab.Screen 
-        name="Home" 
-        component={MainStackNavigator} 
+        name="ActivityFeedScreen" 
+        component={ActivityFeedScreen} 
         options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({ color, size }) => (
@@ -23,8 +25,8 @@ const BottomTabNavigator = () => {
         }}
       />
       <Tab.Screen 
-        name="Search" 
-        component={SearchStackNavigator} 
+        name="SearchScreen" 
+        component={SearchScreen} 
         options={{
           tabBarLabel: 'Search',
           tabBarIcon: ({ color, size }) => (
@@ -33,8 +35,8 @@ const BottomTabNavigator = () => {
         }}
       />
         <Tab.Screen 
-        name="Issues" 
-        component={IssuesStackNavigator} 
+        name="IssuesScreen" 
+        component={IssuesScreen} 
         options={{
           tabBarLabel: 'Issues',
           tabBarIcon: ({ color, size }) => (
@@ -46,4 +48,156 @@ const BottomTabNavigator = () => {
   );
 };
 
-export default BottomTabNavigator;
+const BillInfoCardScreenTab = () => {
+  return (
+    <Tab.Navigator initialRouteName="BillInfoCard"
+    screenOptions={{headerShown: false}}>
+      <Tab.Screen 
+        name="ActivityFeedScreen" 
+        component={ActivityFeedScreen} 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="SearchScreen" 
+        component={SearchScreen} 
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+          ),
+        }}
+      />
+        <Tab.Screen 
+        name="IssuesScreen" 
+        component={IssuesScreen} 
+        options={{
+          tabBarLabel: 'Issues',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="sphere" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const MPInfoCardScreenTab = () => {
+  return (
+    <Tab.Navigator initialRouteName="BillInfoCard"
+    screenOptions={{headerShown: false}}>
+      <Tab.Screen 
+        name="ActivityFeedScreen" 
+        component={ActivityFeedScreen} 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="SearchScreen" 
+        component={SearchScreen} 
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+          ),
+        }}
+      />
+        <Tab.Screen 
+        name="IssuesScreen" 
+        component={IssuesScreen} 
+        options={{
+          tabBarLabel: 'Issues',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="sphere" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const IssuesScreenTab = () => {
+  return (
+    <Tab.Navigator initialRouteName="IssuesScreen"
+    screenOptions={{headerShown: false}}>
+      <Tab.Screen 
+        name="ActivityFeedScreen" 
+        component={ActivityFeedScreen} 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="SearchScreen" 
+        component={SearchScreen} 
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+          ),
+        }}
+      />
+        <Tab.Screen 
+        name="IssuesScreen" 
+        component={IssuesScreen} 
+        options={{
+          tabBarLabel: 'Issues',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="sphere" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+const SearchScreenTab = () => {
+  return (
+    <Tab.Navigator initialRouteName="SearchScreen"
+    screenOptions={{headerShown: false}}>
+      <Tab.Screen 
+        name="ActivityFeedScreen" 
+        component={ActivityFeedScreen} 
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen 
+        name="SearchScreen" 
+        component={SearchScreen} 
+        options={{
+          tabBarLabel: 'Search',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="magnify" color={color} size={size} />
+          ),
+        }}
+      />
+        <Tab.Screen 
+        name="IssuesScreen" 
+        component={IssuesScreen} 
+        options={{
+          tabBarLabel: 'Issues',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="sphere" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+};
+
+export { ActivityFeedTab, MPInfoCardScreenTab, BillInfoCardScreenTab, IssuesScreenTab, SearchScreenTab};
